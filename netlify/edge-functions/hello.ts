8a93e4f2189263d90f7bd07eb098988401aa7273
Mahesh Warnasooriya <mahesh.warnasooriya@bmigroup.com>
2025-07-15 14:14:19 +0100

    add logging

--- netlify/edge-functions/hello.ts
+++ netlify/edge-functions/hello.ts
@@ -2,5 +2,6 @@ import { Context } from "@netlify/edge-functions";
 
 export default async function handler(request: Request, context: Context) {
     console.log("Hello from Netlify Edge Functions!");
+    console.log(`GATSBY_PREVIEW: ${Netlify.env.get("GATSBY_PREVIEW")}`);
     return;
 }
