ad4a2ecb55c1856c21a7a52476539dce79bbbc8a
Mahesh Warnasooriya <mahesh.warnasooriya@bmigroup.com>
2025-07-15 14:34:23 +0100

    scope to production context

--- netlify/edge-functions/hello.ts
+++ netlify/edge-functions/hello.ts
@@ -2,6 +2,5 @@ import { Context } from "@netlify/edge-functions";
 
 export default async function handler(request: Request, context: Context) {
     console.log("Hello from Netlify Edge Functions!");
-    console.log(`GATSBY_PREVIEW: ${Netlify.env.get("GATSBY_PREVIEW")}`);
     return;
 }
