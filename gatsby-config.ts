b8736390b95a1daf3e8d9c443f16320d08f00caa
Mahesh Warnasooriya <mahesh.warnasooriya@bmigroup.com>
2025-07-15 11:23:18 +0100

    add netlify gatsby adaptor

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,10 +1,15 @@
 import type { GatsbyConfig } from "gatsby"
+const adapter = require("gatsby-adapter-netlify").default
 
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `My Gatsby Site`,
     siteUrl: `https://www.yourdomain.tld`,
   },
+  adapter: adapter({
+    excludeDatastoreFromEngineFunction: true,
+    imageCDN: false,
+  }),
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
