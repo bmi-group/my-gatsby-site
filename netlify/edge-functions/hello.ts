10dae96e4148da9d3d0484b95a2e9047bb96e602
Mahesh Warnasooriya <mahesh.warnasooriya@bmigroup.com>
2025-07-16 09:04:35 +0100

    upgrade edge functions package

--- netlify/edge-functions/hello.ts
+++ netlify/edge-functions/hello.ts
@@ -1,6 +1,11 @@
 import { Context } from "@netlify/edge-functions";
 
 export default async function handler(request: Request, context: Context) {
+    if (context.deploy?.context !== "production") {
+    console.log("This is not a production deployment.");   
+    }
+    console.log(`GATSBY_PREVIEW: ${Netlify.env.get("GATSBY_PREVIEW")}`);
+
     console.log("Hello from Netlify Edge Functions!");
     return;
 }
